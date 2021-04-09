762805a2409262634eecbfccbb2340e6a891acef
Rafael Diehl <rafaeljuliani1984@gmail.com>
2021-04-09 17:54:37 -0300

    put profile data on backend

--- src/routes.js
+++ src/routes.js
@@ -3,8 +3,17 @@ const routes = express.Router();
 
 const views = __dirname + "/views/";
 
+const profile = {
+  name: "Rafael",
+  avatar: "https://avatars.githubusercontent.com/u/57677302?v=4",
+  "monthly-budget": 3000,
+  "days-per-week": 5,
+  "hours-per-day": 5,
+  "vacation-per-year": 4
+}
+
 routes.get('/', (req, res) => {
-  return res.render(views + 'index');
+  return res.render(views + 'index', { profile });
 });
 
 routes.get('/job', (req, res) => {
@@ -16,7 +25,7 @@ routes.get('/job/edit', (req, res) => {
 });
 
 routes.get('/profile', (req, res) => {
-  return res.render(views + 'profile');
+  return res.render(views + 'profile', { profile });
 });
 
 module.exports = routes;
