c2a40157342c553f715c85525d5516aaee600c67
Rafael Diehl <rafaeljuliani1984@gmail.com>
2021-04-09 17:22:26 -0300

    create routes

--- src/server.js
+++ src/server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const server = express();
+const routes = require("./routes");
+
 const port = 3000;
 
 // Habilitar arquivos estáticos
 server.use(express.static("public"));
 
-server.get('/', (req, res) => {
-  return res.sendFile(__dirname + "/views/index.html");
-});
+// Habilitar rotas
+server.use(routes);
 
 server.listen(port, () => {
   console.log(`Servidor rodando em: http://localhost:${port}`);
